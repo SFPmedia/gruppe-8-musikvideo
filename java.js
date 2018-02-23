4e40dde9c671de2a6321e4df1e42d88bc5b9a904
SFPmedia <s-pauck@hotmail.com>
2018-02-23 12:03:24 +0100

    IT'S WORKING! JQUERY IS WORKING! YEEEEAAAAAH! THE CODE HAS BEEN CRACKED!

---
+++ java.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+
+  $(".intro img").click(function(){
+    $(".ytikon").hide();
+  });
+  $(".intro img").click(function(){
+    $(".fbikon").hide();
+  });
+  $(".intro img").click(function(){
+    $(".intro h3").hide();
+  });
+  $(".intro img").click(function(){
+    $(this).hide();
+  });
+
+});
